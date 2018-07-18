d50939ed8c22340d573d3bcbec82091a9226cd6d
gcyml <mrwanggc@gmail.com>
2018-07-19 00:57:30 +0800

    String

--- src/java/lang/String.java
+++ src/java/lang/String.java
@@ -1333,43 +1333,33 @@ public final class String
     }
 
     /**
-     * Returns the index within this string of the first occurrence of the
-     * specified character, starting the search at the specified index.
+     * 返回在此字符串中第一次出现指定字符处的索引，从指定的索引开始搜索。
      * <p>
-     * If a character with value {@code ch} occurs in the
-     * character sequence represented by this {@code String}
-     * object at an index no smaller than {@code fromIndex}, then
-     * the index of the first such occurrence is returned. For values
-     * of {@code ch} in the range from 0 to 0xFFFF (inclusive),
-     * this is the smallest value <i>k</i> such that:
+     * 在此 {@code String} 对象表示的字符序列中，
+     * 如果带有值 {@code ch} 的字符的索引不小于 {@code fromIndex}，
+     * 则返回第一次出现该值的索引。对于 0 到 0xFFFF（包括 0 和 0xFFFF）范围内的 {@code ch} 值，返回值是
      * <blockquote><pre>
      * (this.charAt(<i>k</i>) == ch) {@code &&} (<i>k</i> &gt;= fromIndex)
      * </pre></blockquote>
-     * is true. For other values of {@code ch}, it is the
-     * smallest value <i>k</i> such that:
+     * 为 true 的最小<i>k</i>值。
+     * 对于其他 {@code ch} 值，返回值是
      * <blockquote><pre>
      * (this.codePointAt(<i>k</i>) == ch) {@code &&} (<i>k</i> &gt;= fromIndex)
      * </pre></blockquote>
-     * is true. In either case, if no such character occurs in this
-     * string at or after position {@code fromIndex}, then
-     * {@code -1} is returned.
+     * 为 true 的最小 <i>k</i> 值。
+     * 无论哪种情况，
+     * 如果此字符串中 {@code fromIndex} 或之后的位置没有这样的字符出现，则返回 {@code -1}。
      *
      * <p>
-     * There is no restriction on the value of {@code fromIndex}. If it
-     * is negative, it has the same effect as if it were zero: this entire
-     * string may be searched. If it is greater than the length of this
-     * string, it has the same effect as if it were equal to the length of
-     * this string: {@code -1} is returned.
+     * {@code fromIndex} 的值没有限制。如果它为负，则与它为 0 的效果同样：将搜索整个字符串。
+     * 如果它大于此字符串的长度，则与它等于此字符串长度的效果相同：返回 {@code -1}。
      *
-     * <p>All indices are specified in {@code char} values
-     * (Unicode code units).
+     * <p>所有索引都在 {@code char} 值中指定（Unicode 代码单元）。
      *
-     * @param   ch          a character (Unicode code point).
-     * @param   fromIndex   the index to start the search from.
-     * @return  the index of the first occurrence of the character in the
-     *          character sequence represented by this object that is greater
-     *          than or equal to {@code fromIndex}, or {@code -1}
-     *          if the character does not occur.
+     * @param   ch          一个字符（Unicode 代码点）。
+     * @param   fromIndex   开始搜索的索引。
+     * @return 在此对象表示的字符序列中第一次出现的大于或等于 {@code fromIndex} 的字符的索引；
+     *         如果未出现该字符，则返回 {@code -1}。
      */
     public int indexOf(int ch, int fromIndex) {
         final int max = value.length;
@@ -1414,65 +1404,51 @@ public final class String
     }
 
     /**
-     * Returns the index within this string of the last occurrence of
-     * the specified character. For values of {@code ch} in the
-     * range from 0 to 0xFFFF (inclusive), the index (in Unicode code
-     * units) returned is the largest value <i>k</i> such that:
+     * 返回指定字符在此字符串中最后一次出现处的索引。
+     * 对于 0 到 0xFFFF（包括 0 和 0xFFFF）范围内的 {@code ch} 的值，
+     * 返回的索引（Unicode 代码单元）是
      * <blockquote><pre>
      * this.charAt(<i>k</i>) == ch
      * </pre></blockquote>
-     * is true. For other values of {@code ch}, it is the
-     * largest value <i>k</i> such that:
+     * 为 true 的最大<i>k</i>值。
+     * 对于其他 {@code ch} 值，返回值是
      * <blockquote><pre>
      * this.codePointAt(<i>k</i>) == ch
      * </pre></blockquote>
-     * is true.  In either case, if no such character occurs in this
-     * string, then {@code -1} is returned.  The
-     * {@code String} is searched backwards starting at the last
-     * character.
+     * 为 true 的最大<i>k</i>值。
+     * 无论哪种情况，如果此字符串中没有这样的字符出现，则返回 {@code -1}。
+     * 从最后一个字符开始反向搜索此 {@code String}。
      *
-     * @param   ch   a character (Unicode code point).
-     * @return  the index of the last occurrence of the character in the
-     *          character sequence represented by this object, or
-     *          {@code -1} if the character does not occur.
+     * @param   ch   一个字符（Unicode 代码点）。
+     * @return  在此对象表示的字符序列中最后一次出现该字符的索引；
+     *          如果未出现该字符，则返回 {@code -1}。
      */
     public int lastIndexOf(int ch) {
         return lastIndexOf(ch, value.length - 1);
     }
 
     /**
-     * Returns the index within this string of the last occurrence of
-     * the specified character, searching backward starting at the
-     * specified index. For values of {@code ch} in the range
-     * from 0 to 0xFFFF (inclusive), the index returned is the largest
-     * value <i>k</i> such that:
+     * 返回指定字符在此字符串中最后一次出现处的索引，从指定的索引处开始进行反向搜索。
+     * 对于 0 到 0xFFFF（包括 0 和 0xFFFF）范围内的 {@code ch}  值，返回的索引是
      * <blockquote><pre>
      * (this.charAt(<i>k</i>) == ch) {@code &&} (<i>k</i> &lt;= fromIndex)
      * </pre></blockquote>
-     * is true. For other values of {@code ch}, it is the
-     * largest value <i>k</i> such that:
+     * 为 true 的最大<i>k</i>值。
+     * 对于其他 {@code ch} 值，返回值是
      * <blockquote><pre>
      * (this.codePointAt(<i>k</i>) == ch) {@code &&} (<i>k</i> &lt;= fromIndex)
      * </pre></blockquote>
-     * is true. In either case, if no such character occurs in this
-     * string at or before position {@code fromIndex}, then
-     * {@code -1} is returned.
+     * 为 true 的最大<i>k</i>值。
+     * 无论哪种情况，如果此字符串中没有这样的字符出现，则返回 {@code -1}。
      *
-     * <p>All indices are specified in {@code char} values
-     * (Unicode code units).
+     * <p>所有的索引都以 {@code char} 值指定（Unicode 代码单元）。
      *
-     * @param   ch          a character (Unicode code point).
-     * @param   fromIndex   the index to start the search from. There is no
-     *          restriction on the value of {@code fromIndex}. If it is
-     *          greater than or equal to the length of this string, it has
-     *          the same effect as if it were equal to one less than the
-     *          length of this string: this entire string may be searched.
-     *          If it is negative, it has the same effect as if it were -1:
-     *          -1 is returned.
-     * @return  the index of the last occurrence of the character in the
-     *          character sequence represented by this object that is less
-     *          than or equal to {@code fromIndex}, or {@code -1}
-     *          if the character does not occur before that point.
+     * @param   ch          一个字符（Unicode 代码点）。
+     * @param   fromIndex   开始搜索的索引。 {@code fromIndex} 的值没有限制。
+     *          如果它大于等于此字符串的长度，则与它小于此字符串长度减 1 的效果相同：
+     *          将搜索整个字符串。如果它为负，则与它为 -1 的效果相同：返回 -1。
+     * @return 在此对象表示的字符序列（小于等于 {@code fromIndex}）中最后一次出现该字符的索引；
+     *         如果在该点之前未出现该字符，则返回 {@code -1}。
      */
     public int lastIndexOf(int ch, int fromIndex) {
         if (ch < Character.MIN_SUPPLEMENTARY_CODE_POINT) {
